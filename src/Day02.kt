80d249f2d65bf1d902a4947b6e4e9c6c92cc9968
evgenim <e.v.molodtsov@gmail.com>
2023-12-02 14:10:37 +0400

    day2 part2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,26 +1,33 @@
+import kotlin.math.max
+
+fun parseSubset(colorsString: String): IntArray {
+    var red = 0
+    var green = 0
+    var blue = 0
+    for (colorString in colorsString.split(',')) {
+        if (colorString.endsWith("red")) {
+            red += Integer.parseInt(colorString.substring(1, colorString.length - 3).trim())
+        }
+
+        if (colorString.endsWith("green")) {
+            green += Integer.parseInt(colorString.substring(1, colorString.length - 5).trim())
+        }
+
+        if (colorString.endsWith("blue")) {
+            blue += Integer.parseInt(colorString.substring(1, colorString.length - 4).trim())
+        }
+    }
+
+    return intArrayOf(red, green, blue)
+}
+
 fun main() {
 
     fun part1(input: List<String>): Int {
         fun isGameValid(subsets: String): Boolean {
             for (subset in subsets.split(';')) {
-                var red = 0
-                var green = 0
-                var blue = 0
-                for (colorString in subset.split(',')) {
-                    if (colorString.endsWith("red")) {
-                        red+= Integer.parseInt(colorString.substring(1, colorString.length - 3).trim())
-                    }
-
-                    if (colorString.endsWith("green")) {
-                        green+= Integer.parseInt(colorString.substring(1, colorString.length - 5).trim())
-                    }
-
-                    if (colorString.endsWith("blue")) {
-                        blue+= Integer.parseInt(colorString.substring(1, colorString.length - 4).trim())
-                    }
-                }
-
-                if (red > 12 || green > 13 || blue > 14) return false
+                val colors = parseSubset(subset)
+                if (colors[0] > 12 || colors[1] > 13 || colors[2] > 14) return false
             }
 
             return true
@@ -40,12 +47,34 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        fun getMinimalSetPower(subsets: String): Int {
+            var minRed = 0
+            var minGreen = 0
+            var minBlue = 0
+
+            for (subset in subsets.split(';')) {
+                val colors = parseSubset(subset)
+                minRed = max(minRed, colors[0])
+                minGreen = max(minGreen, colors[1])
+                minBlue = max(minBlue, colors[2])
+            }
+
+            return minRed * minGreen * minBlue
+        }
+
+        var sum = 0
+        for (gameString in input) {
+            val gameNumberEnd = gameString.indexOfFirst { it == ':' }
+            sum+= getMinimalSetPower(gameString.substring(gameNumberEnd + 1))
+        }
+
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 8)
+//    check(part1(testInput) == 8)
+    check(part2(testInput) == 2286)
 
     val input = readInput("Day02")
     part1(input).println()
