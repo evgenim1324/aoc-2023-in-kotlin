bc9099982a71019f70b0332c3e7f8dd08125f191
evgenim <e.v.molodtsov@gmail.com>
2023-12-03 14:44:19 +0400

    day3 part2

--- src/Day03.kt
+++ src/Day03.kt
@@ -92,12 +92,88 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        fun String.getDigit(pointer: Int): Int {
+            var start = pointer - 1
+            while (start >= 0) {
+                val c = this[start]
+                if (!c.isDigit()) break
+
+                start--
+            }
+
+            var end = pointer + 1
+            while (end < this.length) {
+                val c = this[end]
+                if (!c.isDigit()) break
+
+                end++
+            }
+
+            return Integer.parseInt(this.substring(start + 1, end))
+        }
+
+        fun String.checkLeftRight(lineNumber: Int, starPosition: Int, digitPositions: ArrayList<Pair<Int, Int>>) {
+            if (starPosition > 0) {
+                val position = starPosition - 1
+                if (this[position].isDigit()) digitPositions.add(lineNumber to position)
+            }
+
+            if (starPosition < this.lastIndex) {
+                val position = starPosition + 1
+                if (this[position].isDigit()) digitPositions.add(lineNumber to position)
+            }
+        }
+
+        fun String.checkLine(lineNumber: Int, position: Int, digitPositions: ArrayList<Pair<Int, Int>>) {
+            if (this[position].isDigit()) {
+                digitPositions.add(lineNumber to position)
+            } else {
+                this.checkLeftRight(lineNumber, position, digitPositions)
+            }
+        }
+
+        fun List<String>.getGearRation(lineNumber: Int, starPosition: Int): Int? {
+            val line = this[lineNumber]
+            val digitPositions = ArrayList<Pair<Int, Int>>()
+
+            line.checkLeftRight(lineNumber, starPosition, digitPositions)
+            if (lineNumber != 0) {
+                val lineToCheck = lineNumber - 1
+                this[lineToCheck].checkLine(lineToCheck, starPosition, digitPositions)
+            }
+            if (lineNumber != this.lastIndex) {
+                val lineToCheck = lineNumber + 1
+                this[lineToCheck].checkLine(lineToCheck, starPosition, digitPositions)
+            }
+
+            if (digitPositions.size != 2) return null
+
+            val digit1 = digitPositions[0]
+            val digit2 = digitPositions[1]
+
+            return this[digit1.first].getDigit(digit1.second) * this[digit2.first].getDigit(digit2.second)
+        }
+
+        var sum = 0
+        for (i in input.indices) {
+            val line = input[i]
+            for (j in line.indices) {
+                if (line[j] == '*') {
+                    val ration = input.getGearRation(i, j)
+                    if (ration != null) {
+                        sum += ration
+                    }
+                }
+            }
+        }
+
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 4361)
+//    check(part1(testInput) == 4361)
+    check(part2(testInput) == 467835)
 
     val input = readInput("Day03")
     part1(input).println()
