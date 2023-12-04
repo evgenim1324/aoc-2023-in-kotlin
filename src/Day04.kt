5e25795644de428cd3bb9ecbb4db33a1b0d5f0d2
evgenim <e.v.molodtsov@gmail.com>
2023-12-04 11:05:28 +0400

    day4 part2

--- src/Day04.kt
+++ src/Day04.kt
@@ -8,11 +8,15 @@ fun main() {
             .map { Integer.parseInt(it) }
     }
 
+    fun getWinningNumbers(line: String): List<Int> {
+        val (winningNumbersString, cardNumbersString) = line.split(':')[1].split('|')
+        val winningNumbers = getNumbers(winningNumbersString).toSet()
+        return getNumbers(cardNumbersString).filter { winningNumbers.contains(it) }.toList()
+    }
+
     fun part1(input: List<String>): Int {
         return input.asSequence().map { line ->
-            val (winningNumbersString, cardNumbersString) = line.split(':')[1].split('|')
-            val winningNumbers = getNumbers(winningNumbersString).toSet()
-            val cardWinningNumbers = getNumbers(cardNumbersString).filter { winningNumbers.contains(it) }.toList()
+            val cardWinningNumbers = getWinningNumbers(line)
 
             if (cardWinningNumbers.isEmpty()) return@map 0
 
@@ -26,12 +30,23 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var counter = 0
+        val cards = IntArray(input.size) { 1 }
+        for ((i, line) in input.withIndex()) {
+            val winning = getWinningNumbers(line).size
+            val numberOfCards = cards[i]
+            for (j in i + 1 .. (i + winning).coerceAtMost(cards.lastIndex)) cards[j]+= numberOfCards
+
+            counter+= numberOfCards
+        }
+
+        return counter
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
-    check(part1(testInput) == 13)
+//    check(part1(testInput) == 13)
+    check(part2(testInput) == 30)
 
     val input = readInput("Day04")
     part1(input).println()
