fee6ccf1de120b2daa16139e3ddc790852a8d822
evgenim <e.v.molodtsov@gmail.com>
2023-12-05 18:35:17 +0400

    day5 part2

--- src/Day05.kt
+++ src/Day05.kt
@@ -2,10 +2,7 @@ import java.util.TreeMap
 
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        val seeds = input[0].substring(6).readNumbers()
-        check(input[1].isBlank())
-
+    fun readAlmanac(input: List<String>): Array<Pair<String, TreeMap<Long, Pair<Long, Long>>>> {
         var linePointer = 2
         fun checkHeader(title: String) {
             if (linePointer >= input.size) throw IllegalArgumentException("Title $title not found !")
@@ -43,36 +40,112 @@ fun main() {
             map.readTableLines()
         }
 
-        fun TreeMap<Long, Pair<Long, Long>>.mapItem(item: Long): Long {
-            val (source, destinationData) = this.floorEntry(item)?: return item
-            val (destination, length) = destinationData
-            if (item < source + length) {
-                return destination + (item - source)
+        return maps
+    }
+
+    fun Map.Entry<Long, Pair<Long, Long>>.sourceRange(): LongRange {
+        val (source, destinationData) = this
+        val (_, length) = destinationData
+
+        return source ..< source + length
+    }
+
+    fun LongRange.translateSeedRange(entry: Map.Entry<Long, Pair<Long, Long>>): LongRange {
+        val (source, destinationData) = entry
+        val (destination, _) = destinationData
+
+        val firstInDestination = destination + (this.first - source)
+
+        return firstInDestination.. firstInDestination + (this.last - this.first)
+    }
+
+    fun TreeMap<Long, Pair<Long, Long>>.mapItem(item: LongRange): List<LongRange> {
+        val points = mutableListOf<LongRange>()
+        var range: LongRange? = item
+        while (range != null) {
+            val floorEntry = this.floorEntry(range.first)
+            val floorRange = floorEntry?.sourceRange()
+            if (floorRange == null || !floorRange.contains(range.first)) {
+                val ceilingEntry = this.ceilingEntry(range.first)
+                if (ceilingEntry == null) {
+                    points.add(range)
+                    range = null
+                    continue
+                }
+
+                val ceilingRange = ceilingEntry.sourceRange()
+                if (!ceilingRange.contains(range.last)) {
+                    points.add(range)
+                    range = null
+                    continue
+                }
+
+                points.add(range.first..< ceilingRange.first)
+                range = ceilingRange.first .. range.last
+                continue
             }
 
-            return item
-        }
-
-        fun Array<Pair<String, TreeMap<Long, Pair<Long, Long>>>>.mapFromSeedToLocation(seed: Long): Long {
-            var item = seed
-            for ((_, map) in this) {
-                item = map.mapItem(item)
+            if (floorRange.contains(range.last)) {
+                points.add(range.translateSeedRange(floorEntry))
+                range = null
+                continue
             }
 
-            return item
+            val ceilingEntry = this.ceilingEntry(range.first)
+            if (ceilingEntry == null)  {
+                points.add((range.first  .. floorRange.last).translateSeedRange(floorEntry))
+                range = floorRange.last + 1 .. range.last
+                continue
+            }
+
+            val ceilingRange = ceilingEntry.sourceRange()
+            if (ceilingRange.contains(range.last)) {
+                points.add((range.first  ..< ceilingRange.first).translateSeedRange(floorEntry))
+                range = ceilingRange.first .. range.last
+                continue
+            }
+
+            points.add((range.first  .. floorRange.last).translateSeedRange(floorEntry))
+            range = floorRange.last + 1 .. range.last
         }
 
+        return points
+    }
 
-        return seeds.map { maps.mapFromSeedToLocation(it) }.min().toInt()
+    fun Array<Pair<String, TreeMap<Long, Pair<Long, Long>>>>.mapFromSeedToLocation(seed: LongRange): List<LongRange> {
+        var item = listOf(seed)
+        for ((_, map) in this) {
+            item = item.flatMap { map.mapItem(it) }.toList()
+        }
+
+        return item
+    }
+
+    fun part1(input: List<String>): Int {
+        val seeds = input[0].substring(6).readNumbers()
+        check(input[1].isBlank())
+
+        val maps = readAlmanac(input)
+
+        return seeds.map { maps.mapFromSeedToLocation(it .. it).asSequence().map { it.first }.min() }.min().toInt()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val seeds = input[0].substring(6).readNumbers().windowed(2, 2)
+        check(input[1].isBlank())
+
+        val maps = readAlmanac(input)
+
+        return seeds
+            .map {
+                maps.mapFromSeedToLocation(it[0] ..< (it[0] + it[1])).asSequence().map { it.first }.min()
+            }.min().toInt()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
     check(part1(testInput) == 35)
+    check(part2(testInput) == 46)
 
 
     val input = readInput("Day05")
