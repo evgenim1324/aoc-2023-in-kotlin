a2853c9db5d27465479a8caf60e3775a22a70dec
evgenim <e.v.molodtsov@gmail.com>
2023-12-08 09:25:25 +0400

    day8 part1

---
+++ src/Day08.kt
@@ -0,0 +1,49 @@
+fun main() {
+
+    fun String.readNavigation(): List<Int> {
+        val characters = mapOf('L' to 0, 'R' to 1)
+        return this.map { characters[it]!! }
+    }
+
+    fun String.readInstruction() : Pair<String, Array<String>> {
+        val (key, value1, value2) = this.split('=', ',')
+        return key.trim() to arrayOf(value1.trim(' ', '('), value2.trim(' ', ')'))
+    }
+
+    fun part1(input: List<String>): Int {
+        val navigation = input[0].readNavigation()
+        var navigationPointer = 0
+
+        fun nextMove(): Int {
+            if (navigationPointer >= navigation.size) navigationPointer = 0
+
+            return navigation[navigationPointer]
+                .also { navigationPointer++ }
+        }
+
+        val rules = input.asSequence().drop(2).map { it.readInstruction() }.associate { it }
+
+        var currentRule = "AAA"
+
+        var counter = 0
+
+        while (currentRule != "ZZZ") {
+            currentRule = rules[currentRule]!![nextMove()]
+            counter++
+        }
+
+        return counter
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day08_test")
+    check(part1(testInput) == 6)
+
+    val input = readInput("Day08")
+    part1(input).println()
+    part2(input).println()
+}
