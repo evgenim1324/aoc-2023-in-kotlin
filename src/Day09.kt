4ff2980e3ce76922050005fbb2f5d64e0c2977aa
evgenim <e.v.molodtsov@gmail.com>
2023-12-09 16:28:00 +0400

    day9 part2

--- src/Day09.kt
+++ src/Day09.kt
@@ -9,17 +9,27 @@ fun main() {
         return this.last() + nextSequence.getNextValue()
     }
 
+    tailrec fun Sequence<Long>.getPreviousValue(): Long {
+        val nextSequence = this.windowed(2).map { it[1] - it[0] }
+        if (nextSequence.all { it == 0L }) {
+            return this.first()
+        }
+
+        return this.first() - nextSequence.getPreviousValue()
+    }
+
     fun part1(input: List<String>): Int {
         return input.asSequence().map { it.readNumbers().getNextValue().toInt() }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.asSequence().map { it.readNumbers().getPreviousValue().toInt()}.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day09_test")
     check(part1(testInput) == 114)
+    check(part2(testInput) == 2)
 
     val input = readInput("Day09")
     part1(input).println()
