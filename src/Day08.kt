cc55bf54db6691fb9c3eacaaa3601aeb45d1a5f0
evgenim <e.v.molodtsov@gmail.com>
2023-12-09 16:58:11 +0400

    day8 part2

--- src/Day08.kt
+++ src/Day08.kt
@@ -35,13 +35,46 @@ fun main() {
         return counter
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val navigation = input[0].readNavigation()
+        var navigationPointer = 0
+
+        fun nextMove(): Int {
+            if (navigationPointer >= navigation.size) navigationPointer = 0
+
+            return navigation[navigationPointer]
+                .also { navigationPointer++ }
+        }
+
+        val rulesParsed = input.asSequence().drop(2).map { it.readInstruction() }.toList()
+
+        val rules = rulesParsed.associate { it }
+
+        var currentRule = rulesParsed.filter { it.first[2] == 'A'}.map { it.first }.toList()
+
+        fun getMovesForRule(start: String): Long {
+            navigationPointer = 0
+
+            var currentRule = start
+
+            var counter = 0L
+
+            while (currentRule[2] != 'Z') {
+                currentRule = rules[currentRule]!![nextMove()]
+
+                counter++
+            }
+
+//            println(counter)
+            return counter
+        }
+
+        return currentRule.map { getMovesForRule(it).toBigInteger() }.reduce { acc, c -> acc * c / acc.gcd(c) }.toLong()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day08_test")
-    check(part1(testInput) == 6)
+    check(part1(readInput("Day08_test")) == 6)
+    check(part2(readInput("Day08_test2")) == 6L)
 
     val input = readInput("Day08")
     part1(input).println()
