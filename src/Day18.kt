1763a665f3e05a6099020f7bcd7afa567cf09d13
evgenim <e.v.molodtsov@gmail.com>
2023-12-19 00:13:34 +0400

    day18 part2

--- src/Day18.kt
+++ src/Day18.kt
@@ -6,8 +6,6 @@ enum class LavaTrenchDirection {
     U, D, L, R
 }
 
-class LavaTrenchPoint(x: Int, y: Int)
-
 fun main() {
 
     fun getPath(input: List<String>) = input.map {
@@ -15,32 +13,14 @@ fun main() {
         LavaTrenchDirection.valueOf(direction) to number.toInt()
     }
 
-    fun borderChar(prev: LavaTrenchDirection, next: LavaTrenchDirection): Char {
-        return when(prev to next) {
-            LavaTrenchDirection.L to LavaTrenchDirection.L -> '━'
-            LavaTrenchDirection.R to LavaTrenchDirection.R -> '━'
-            LavaTrenchDirection.D to LavaTrenchDirection.D -> '┃'
-            LavaTrenchDirection.U to LavaTrenchDirection.U -> '┃'
-            LavaTrenchDirection.U to LavaTrenchDirection.R -> '┌'
-            LavaTrenchDirection.U to LavaTrenchDirection.L -> '┑'
-            LavaTrenchDirection.D to LavaTrenchDirection.R -> '┕'
-            LavaTrenchDirection.D to LavaTrenchDirection.L -> '┙'
-            LavaTrenchDirection.L to LavaTrenchDirection.D -> '┌'
-            LavaTrenchDirection.L to LavaTrenchDirection.U -> '┕'
-            LavaTrenchDirection.R to LavaTrenchDirection.D -> '┑'
-            LavaTrenchDirection.R to LavaTrenchDirection.U -> '┙'
-            else -> throw IllegalArgumentException()
-        }
-    }
+    fun List<Pair<LavaTrenchDirection, Int>>.getArea(): Long {
+        var maxWidth = 0L
+        var minWidth = 0L
+        var maxHight = 0L
+        var minHight = 0L
 
-    fun List<Pair<LavaTrenchDirection, Int>>.getMap(): Array<CharArray> {
-        var maxWidth = 0
-        var minWidth = 0
-        var maxHight = 0
-        var minHight = 0
-
-        var width = 0
-        var hight = 0
+        var width = 0L
+        var hight = 0L
         this.forEach { (d, n) ->
             when (d) {
                 LavaTrenchDirection.R -> width+=n
@@ -58,77 +38,65 @@ fun main() {
         width = maxWidth - minWidth + 1
         hight = maxHight - minHight + 1
 
-        val map = Array (hight) { CharArray(width) { '*' } }
-
-        var x = hight - minHight.absoluteValue - 1
-        var y = minWidth.absoluteValue
-
-        var prev = this.last().first
-
+        var s = 0L
+        var h = hight
+        var l = 0L
         for (i in this.indices) {
             val (d, n) = this[i]
-            map[x][y] = borderChar(prev, d)
-
             when (d) {
                 LavaTrenchDirection.R -> {
-                    repeat(n - 1) { map[x][++y] = '━' }; y++
+                    s += n * h
                 }
 
                 LavaTrenchDirection.L -> {
-                    repeat(n - 1) { map[x][--y] = '━' }; y--
+                    s -= n * h
                 }
 
                 LavaTrenchDirection.U -> {
-                    repeat(n - 1) { map[--x][y] = '┃' }; x--
+                    h += n
                 }
 
                 LavaTrenchDirection.D -> {
-                    repeat(n - 1) { map[++x][y] = '┃' }; x++
+                    h -= n
                 }
             }
 
-            prev = d
+            l += n
         }
 
-        return map
-    }
 
-    fun Array<CharArray>.countSpace(): Int {
-        val bars = arrayOf('┃' , '┌', '┑')
-
-        var counter = 0
-        for (row in this) {
-            var barCounter = 0
-            for (cell in row) {
-                if (cell != '*') {
-                    if (cell in bars) barCounter++
-
-                    counter++
-                    continue
-                }
-
-                if (barCounter % 2 != 0) counter++
-            }
-        }
-
-        return counter
+        return s.absoluteValue + l / 2 + 1
     }
 
 
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<String>): Long {
         val path = getPath(input)
-        val map = path.getMap()
-
-        return map.countSpace()
+        return path.getArea()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun getPath2(input: List<String>) = input.map {
+        val (_, number) = it.split('#')
+        val n = Integer.parseInt(number.substring(0, 5), 16)
+        // dig: 0 means R, 1 means D, 2 means L, and 3 means U.
+        val d = when (number.substring(5, 6)) {
+            "0" -> LavaTrenchDirection.R
+            "1" -> LavaTrenchDirection.D
+            "2" -> LavaTrenchDirection.L
+            "3" -> LavaTrenchDirection.U
+            else -> throw IllegalArgumentException()
+        }
+        d to n
+    }
+
+    fun part2(input: List<String>): Long {
+        val path = getPath2(input)
+        return path.getArea()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day18_test")
-    check(part1(testInput) == 62)
+    check(part1(testInput) == 62L)
+    check(part2(testInput) == 952408144115)
 
     val input = readInput("Day18")
     part1(input).println()
