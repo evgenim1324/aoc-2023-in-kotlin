97a24ac8fadcc7cbd3fdce49396bf5ce3e878c26
evgenim <e.v.molodtsov@gmail.com>
2023-12-21 00:38:40 +0400

    day20 part2

--- src/Day20.kt
+++ src/Day20.kt
@@ -1,32 +1,20 @@
-
 data class Signal(
     val from: String,
     val to: String,
     val value: Boolean
 )
 
-data class TotalSignals (
-    val low: Long,
-    val high: Long
-)
-
 class Machine(
-    val devices: Map<String, Device>
+    val devices: Map<String, Device>,
+    val onSignal: (Signal) -> Unit
 ) {
     internal val signalQueue = ArrayDeque<Signal>()
 
-    fun pushButton(): TotalSignals {
+    fun pushButton() {
         signalQueue.add(Signal("button", "broadcaster", false))
-        var lowSignals = 0L
-        var highSignals = 0L
         while (signalQueue.isNotEmpty()) {
             val signal = signalQueue.removeFirst()
-            if (signal.value)
-                highSignals++
-            else
-                lowSignals++
-
-            println("${signal.from} -${if (signal.value) "high" else "low"}-> ${signal.to}")
+            onSignal(signal)
             val device = devices[signal.to] ?: continue // output
             when(device) {
                 is Device.Broadcaster -> device.receive(signal.value)
@@ -34,8 +22,6 @@ class Machine(
                 is Device.Conjunction -> device.receive(signal.from, signal.value)
             }
         }
-
-        return TotalSignals(lowSignals, highSignals)
     }
 }
 
@@ -75,10 +61,10 @@ sealed interface Device {
 
     class Conjunction(
         override val name: String,
-        private val connections: List<String>,
+        val connections: List<String>,
         input: List<String>
     ) : Device {
-        private val memory = input.associateWithTo(mutableMapOf()) { false }
+        val memory = input.associateWithTo(mutableMapOf()) { false }
 
         context(Machine)
         fun receive(input: String, signal: Boolean) {
@@ -92,7 +78,7 @@ sealed interface Device {
     }
 }
 
-fun buildMachine(input: List<String>): Machine {
+fun buildDevices(input: List<String>): Map<String, Device> {
     fun String.toConnections() = this.split(",").asSequence().map(String::trim).toList()
 
     val inputConnections = mutableMapOf<String, MutableList<String>>()
@@ -148,34 +134,60 @@ fun buildMachine(input: List<String>): Machine {
         devices[conjunction.first] = Device.Conjunction(conjunction.first, conjunction.second, inputConnections[conjunction.first]!!)
     }
 
-    return Machine(devices)
+    return devices
 }
 
 fun main() {
 
     fun part1(input: List<String>): Long {
-        val machine = buildMachine(input)
+        val devices = buildDevices(input)
 
         var lowTotal = 0L
         var highTotal = 0L
-        repeat(1000) {
-            val (low, high) = machine.pushButton()
-            lowTotal+=low
-            highTotal+=high
+
+        val machine = Machine(devices) { signal ->
+              println("${signal.from} -${if (signal.value) "high" else "low"}-> ${signal.to}")
+            if (signal.value)
+                highTotal++
+            else
+                lowTotal++
         }
+
+        repeat(1000) { machine.pushButton() }
 
         return lowTotal * highTotal
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val devices = buildDevices(input)
+        val finalConjunction = devices.values.asSequence().filterIsInstance<Device.Conjunction>().find { it.connections[0] == "rx" }!!
+
+        val counters = finalConjunction.memory.keys.asSequence().associateWithTo(mutableMapOf()) { 0L }
+
+        var counter = 1L
+
+        val machine = Machine(devices) { signal ->
+            if (signal.to == finalConjunction.name && signal.value) {
+                counters[signal.from] = counter
+            }
+        }
+
+        while (counters.any { (_, v) -> v == 0L}) {
+            machine.pushButton()
+            counter++
+        }
+
+        return counters.values.map { it.toBigInteger() }.reduce { acc, c -> acc * c / acc.gcd(c) }.toLong()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day20_test")
     check(part1(testInput) == 32000000L)
+//
+//    val testInput2 = readInput("Day20_output_test")
+//    check(part1(testInput2) == 32000000L)
 
     val input = readInput("Day20")
-    part1(input).println()
+    part2(input).println()
     part2(input).println()
 }
